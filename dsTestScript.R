016abbc1dac80f514b3690559f36127123e12a67
Thomas Burton <twyburton@outlook.com>
2014-12-16 14:57:37 +0000

    Added test script

---
+++ dsTestScript.R
@@ -0,0 +1,23 @@
+# Load opal and datashield libraries
+library(opal)
+library(opaladmin)
+library(dsBaseClient)
+library(dsStatsClient)
+library(dsGraphicsClient)
+
+# Login details
+server <- "Single Site Test"
+url <- "192.168.56.105"
+user <- "administator"
+password <- "password"
+table <- "SingleSiteTest.CNSIM"
+
+# Create a dataframe with all these details
+logindata <- data.frame(server,url,user,password,table)
+opals <- datashield.login(logins=logindata, assign = TRUE)
+
+
+ds.mean("D$LAB_HDL",type="combine")
+
+
+datashield.logout(opals)
